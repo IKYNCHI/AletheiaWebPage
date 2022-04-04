b651e1b6eef0fe70863cfc493eaa50887e23ffe4
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-04-04 15:11:41 +0800

    Implemented simple GraphQL Backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "aletheiawebpage": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/pages/bmpage.js
+++ src/pages/bmpage.js
@@ -1,12 +1,64 @@
-import React from 'react'
-import Progress from '../components/benchmark'
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import { API } from 'aws-amplify';
+import { listTodos } from '../graphql/queries';
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from '../graphql/mutations';
 
-const bmpage = () => {
+const initialFormState = { name: '', description: '' }
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteTodo({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
   return (
-    <>
-    <Progress/>
-    </>
-  )
+    <div className="App">
+      <h1>Aletheia Back-end Test</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createTodo}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteTodo(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 }
 
-export default bmpage
+export default (App);
