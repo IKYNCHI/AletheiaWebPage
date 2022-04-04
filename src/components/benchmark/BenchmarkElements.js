d0ebf9baee5b146db8c70a38d10ee198697987cb
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-04-04 09:54:21 +0800

    backend interface test

--- src/components/benchmark/BenchmarkElements.js
+++ src/components/benchmark/BenchmarkElements.js
@@ -1,78 +1,66 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-export const Container = styled.div`
-    min-height: 692px;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 0;
-    overflow: hidden;
-    background: #0466C8;
-`
-export const FormWrap = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+const initialFormState = { name: '', description: '' }
 
-    @media screen and (max-width: 400px){
-        height: 80%;
-    }
-`
-export const Icon = styled(Link)`
-    margin-left: 32px;
-    margin-top: 32px;
-    text-decoration: none;
-    color: #fff;
-    font-weight: 700;
-    font-size: 32px;
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
 
-    @media screen and (max-width: 480px){
-        margin-left: 16px;
-        margin-top: 8px;
-    }
-` 
-export const FormContent = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-    @media screen and (max-width: 480px){
-        padding: 10px;
-    }
-`
-export const Form = styled.form`
-    background: #001233;
-    max-width: 400px;
-    height: auto;
-    width: 100%;
-    z-index: 1;
-    display: grid;
-    margin: 0 auto;
-    padding: 80px 32px;
-    border-radius: 4px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0, 0.9);
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
 
-    @media screen and (max-width: 480px){
-        padding: 32px 32px;
-    }
-`
-export const FormInput = styled.input`
-    padding: 16px 16px;
-    margin-bottom: 32px;
-    border: none;
-    border-radius: 4px;
-`
-export const FormButton = styled.button`
-    background: #001233;
-    padding: 16px 0;
-    border: none;
-    border-radius: 4px;
-    color: #fff;
-    font-size: 20px;
-    cursor: pointer;
-`
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
