64ef79e58aa0ff738c50746daadb09f14aef0723
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-04-05 22:37:24 +0800

    Implemented simple GraphQL Backend

--- src/pages/bmpage.js
+++ src/pages/bmpage.js
@@ -5,6 +5,11 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listTodos } from '../graphql/queries';
 import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from '../graphql/mutations';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
+
 const initialFormState = { name: '', description: '' }
 
 function App() {
