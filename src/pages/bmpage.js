c12a53721e8858545037e6f3b1ec75a08e848bed
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-04-06 09:10:46 +0800

    restructured Schema

--- src/pages/bmpage.js
+++ src/pages/bmpage.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listTodos } from '../graphql/queries';
-import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from '../graphql/mutations';
+import { listNotes } from '../graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from '../graphql/mutations';
 
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from '../aws-exports';
@@ -20,18 +20,18 @@ function App() {
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({ query: listTodos });
+    const apiData = await API.graphql({ query: listNotes });
     setNotes(apiData.data.listNotes.items);
   }
 
-  async function createTodo() {
+  async function createNote() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
 
-  async function deleteTodo({ id }) {
+  async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
     await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
@@ -50,14 +50,14 @@ function App() {
         placeholder="Note description"
         value={formData.description}
       />
-      <button onClick={createTodo}>Create Note</button>
+      <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
         {
           notes.map(note => (
             <div key={note.id || note.name}>
               <h2>{note.name}</h2>
               <p>{note.description}</p>
-              <button onClick={() => deleteTodo(note)}>Delete note</button>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
             </div>
           ))
         }
